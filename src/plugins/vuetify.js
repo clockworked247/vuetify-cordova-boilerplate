39af53f51f6763476729193c1a93fc4e869c3b27
Antoine <antoine.lozach@imie.fr>
2019-02-15 15:28:55 +0100

    add vuetify and cordova

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import fr from 'vuetify/es5/locale/fr'
+
+Vue.use(Vuetify, {
+  customProperties: true,
+  iconfont: 'fa',
+  lang: {
+    locales: { fr },
+    current: 'fr'
+  },
+})
